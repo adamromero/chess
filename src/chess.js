c154d44d3d6cbcec987bc49294b37cbc9b5f373f
Adam Romero <adamromero89@gmail.com>
2020-01-08 00:03:12 -0800

    Add highlighted moves for rook and bishop

--- src/chess.js
+++ src/chess.js
@@ -1,131 +1,189 @@
 import "./style.scss";
-//import { boardArray } from "./boardArray.js";
 
 const board = document.querySelectorAll(".cell");
 const pieces = document.querySelectorAll(".piece");
 const BOARD_SIZE = 8;
-let currentPiece;
 
-function piecePositionCheck(piece, pieceIndex, cellIndex) {
-   let validMove = false;
+function isCellEmpty(cell) {
+   return !cell.innerHTML.trim().length;
+}
 
+function highlightPossibleMoves(currentPiece) {
+   const piece = currentPiece.classList[1];
+   const pieceIndex = parseInt(currentPiece.parentNode.getAttribute("index"));
+
+   /*
    const cellPosition = {
       cellX: cellIndex % BOARD_SIZE,
       cellY: Math.floor(cellIndex / BOARD_SIZE)
-   };
+   };*/
 
    const piecePosition = {
       cellX: pieceIndex % BOARD_SIZE,
       cellY: Math.floor(pieceIndex / BOARD_SIZE)
    };
 
-   if (piece === "wp") {
-      if (pieceIndex - 8 === cellIndex || pieceIndex - 16 === cellIndex) {
-         validMove = true;
-      }
-   }
-
-   if (piece === "bp") {
-      if (pieceIndex + 8 === cellIndex || pieceIndex + 16 === cellIndex) {
-         validMove = true;
-      }
-   }
-
-   //bishop check
-   if (piece === "wb" || piece === "bb") {
-      console.log("cell y: ", cellPosition.cellY);
-      console.log("cell x: ", cellPosition.cellX);
-      console.log("piece y: ", piecePosition.cellY);
-      console.log("piece x: ", piecePosition.cellX);
-      if (
-         cellPosition.cellY !== piecePosition.cellY &&
-         cellPosition.cellX !== piecePosition.cellX
-      ) {
-         validMove = true;
-      }
-   }
-
-   //rook check
-   if (piece === "wr" || piece === "br") {
-      if (
-         cellPosition.cellY === piecePosition.cellY ||
-         cellPosition.cellX === piecePosition.cellX
-      ) {
-         validMove = true;
-      }
-   }
-
-   // queen check
-   if (piece === "wq" || piece === "bq") {
-      if (
-         cellPosition.cellY === piecePosition.cellY ||
-         cellPosition.cellX === piecePosition.cellX ||
-         (cellIndex - pieceIndex) % 9 === 0 ||
-         (cellIndex - pieceIndex) % 7 === 0 ||
-         (cellIndex + pieceIndex) % 9 === 0 ||
-         (cellIndex + pieceIndex) % 7 === 0
-      ) {
-         validMove = true;
-      }
-   }
-
-   return validMove;
-}
-
-function isValidMove(currentPiece, cell) {
-   const piece = currentPiece.classList[1];
-   const pieceIndex = parseInt(currentPiece.parentNode.getAttribute("index"));
-   const cellIndex = parseInt(cell.getAttribute("index"));
-
-   return isCellEmpty(cell) && piecePositionCheck(piece, pieceIndex, cellIndex);
-}
-
-function isCellEmpty(cell) {
-   return !cell.innerHTML.trim().length;
-}
-
-function dragDrop() {
-   if (isValidMove(currentPiece, this)) {
-      console.log("its valid");
-      this.append(currentPiece);
-   } else {
-      this.classList.remove("hovered");
-   }
-}
-
-///////////////////////////////////////////////////////////////////////////
-
-function getValidMove() {
-   const piece = currentPiece.classList[1];
-   const pieceIndex = parseInt(currentPiece.parentNode.getAttribute("index"));
+   //console.log("pieceindex: ", pieceIndex);
 
    if (piece === "wp") {
-      board[pieceIndex - 8].classList.add("highlight");
-      board[pieceIndex - 16].classList.add("highlight");
+      if (isCellEmpty(board[pieceIndex - 8])) {
+         board[pieceIndex - 8].classList.add("highlight");
+
+         if (isCellEmpty(board[pieceIndex - 16])) {
+            board[pieceIndex - 16].classList.add("highlight");
+         }
+      }
+   }
+
+   //console.log("cell x: ", cellPosition.cellX);
+   //console.log("cell y: ", cellPosition.cellY);
+   console.log("piece cell x: ", piecePosition.cellX);
+   console.log("piece cell y: ", piecePosition.cellY);
+
+   if (piece === "wb") {
+      console.log(piecePosition.cellX - 1);
+      console.log(piecePosition.cellY - 1);
+
+      setDiagonalPositions(piecePosition);
+   }
+
+   if (piece === "wr") {
+      setHorizontalVerticalPositions(piecePosition);
    }
 }
 
-function highlightPossibleMoves() {
-   getValidMove();
+function setHorizontalVerticalPositions(position) {
+   let x = position.cellX,
+      y = position.cellY;
+
+   while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+      y--;
+      if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+         board[y * 8 + x].classList.add("highlight");
+      }
+   }
+
+   (x = position.cellX), (y = position.cellY);
+
+   while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+      y++;
+      if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+         board[y * 8 + x].classList.add("highlight");
+      }
+   }
+
+   (x = position.cellX), (y = position.cellY);
+
+   while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+      x--;
+      if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+         board[y * 8 + x].classList.add("highlight");
+      }
+   }
+
+   (x = position.cellX), (y = position.cellY);
+
+   while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+      x++;
+      if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+         board[y * 8 + x].classList.add("highlight");
+      }
+   }
 }
 
-function clearHighlights() {
+function setDiagonalPositions(position) {
+   let x = position.cellX,
+      y = position.cellY;
+
+   //diagonal left up
+   while (x >= 0 && y >= 0) {
+      x--;
+      y--;
+      if (x >= 0 && y >= 0) {
+         board[y * 8 + x].classList.add("highlight");
+      }
+   }
+
+   (x = position.cellX), (y = position.cellY);
+
+   //diagonal right up
+   while (x < 8 && y >= 0) {
+      x++;
+      y--;
+
+      if (x < 8 && y >= 0) {
+         board[y * 8 + x].classList.add("highlight");
+      }
+   }
+
+   (x = position.cellX), (y = position.cellY);
+
+   //diagonal right down
+   while (x < 8 && y < 8) {
+      x++;
+      y++;
+      if (x < 8 && y < 8) {
+         board[y * 8 + x].classList.add("highlight");
+      }
+   }
+
+   (x = position.cellX), (y = position.cellY);
+
+   //diagonal left down
+   while (x >= 0 && y < 8) {
+      x--;
+      y++;
+      if (x >= 0 && y < 8) {
+         board[y * 8 + x].classList.add("highlight");
+      }
+   }
+}
+
+function selectMove(currentPiece) {
+   const possibleMoves = document.querySelectorAll(".highlight");
+
+   possibleMoves.forEach(function(target) {
+      target.addEventListener("click", function() {
+         const cellIndex = parseInt(this.getAttribute("index"));
+         const pieceIndex = parseInt(
+            currentPiece.parentNode.getAttribute("index")
+         );
+
+         if (cellIndex !== pieceIndex) {
+            this.append(currentPiece);
+            clearSelection();
+         }
+      });
+   });
+}
+
+function clearSelection() {
    board.forEach(function(cell) {
       cell.classList.remove("highlight");
+      cell.classList.remove("selected");
    });
 }
 
 const bindMovement = () => {
    pieces.forEach(function(piece) {
       piece.addEventListener("click", function() {
-         if (piece.classList[2] === "highlight") {
-            this.classList.remove("highlight");
+         const currentPiece = this;
+         if (currentPiece.parentNode.classList.contains("selected")) {
+            clearSelection();
          } else {
-            clearHighlights();
-            currentPiece = this;
-            this.classList.add("highlight");
-            highlightPossibleMoves();
+            clearSelection();
+            currentPiece.parentNode.classList.add("selected");
+            highlightPossibleMoves(currentPiece);
+            selectMove(currentPiece);
          }
+      });
+
+      piece.addEventListener("mouseover", function() {
+         this.parentNode.classList.add("highlight");
+      });
+
+      piece.addEventListener("mouseout", function() {
+         this.parentNode.classList.remove("highlight");
       });
    });
 };
