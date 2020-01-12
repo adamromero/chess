de47c1b59e7042be3aa25efcd34a55d851a4f2a9
Adam Romero <adamromero89@gmail.com>
2020-01-11 19:53:54 -0800

    Add ability to determine check on king when other pieces move

--- chess.js
+++ chess.js
@@ -4,6 +4,13 @@ const Chess = (function() {
    const BOARD_SIZE = 8;
    let whiteTurn = true;
 
+   function convertIndexToCoordinates(index) {
+      return {
+         x: index % BOARD_SIZE,
+         y: Math.floor(index / BOARD_SIZE)
+      };
+   }
+
    function convertCoordinatesToIndex(x, y) {
       if (coordinatesInBounds(x, y)) {
          return y * BOARD_SIZE + x;
@@ -15,45 +22,46 @@ const Chess = (function() {
       return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
    }
 
+   function setPawnPositionsByColor(x, y, color) {
+      let forward, left, right, twoForward, row;
+      if (color === "w") {
+         row = 6;
+         forward = [0, -1];
+         left = [-1, -1];
+         right = [1, -1];
+         twoForward = [0, -2];
+      } else {
+         row = 1;
+         forward = [0, 1];
+         left = [-1, 1];
+         right = [1, 1];
+         twoForward = [0, 2];
+      }
+      //prevent attack from the front
+      if (!cellIsTakenByOpponent(x, y + forward[1])) {
+         setHighlightOnValidCells(x, y, forward, false);
+      }
+      //allow diagonal attacks
+      if (cellIsTakenByOpponent(x + left[0], y + left[1])) {
+         setHighlightOnValidCells(x, y, left, false);
+      }
+      if (cellIsTakenByOpponent(x + right[0], y + right[1])) {
+         setHighlightOnValidCells(x, y, right, false);
+      }
+      //first move for pawns have option of moving two spaces ahead
+      if (y === row) {
+         //prevent two tile jump when a piece is in front of it
+         if (cellIsEmpty(x, y + forward[1])) {
+            setHighlightOnValidCells(x, y, twoForward, false);
+         }
+      }
+   }
+
    function setPawnPositions(x, y, color) {
       if (color === "w") {
-         //prevent attack from the front
-         if (!cellIsTakenByOpponent(x, y - 1)) {
-            setHighlightOnValidCells(x, y, [0, -1], false);
-         }
-         //allow diagonal attacks
-         if (cellIsTakenByOpponent(x - 1, y - 1)) {
-            setHighlightOnValidCells(x, y, [-1, -1], false);
-         }
-         if (cellIsTakenByOpponent(x + 1, y - 1)) {
-            setHighlightOnValidCells(x, y, [1, -1], false);
-         }
-         //first move for pawns have option of moving two spaces ahead
-         if (y === 6) {
-            //prevent two tile jump when a piece is in front of it
-            if (cellIsEmpty(x, y - 1)) {
-               setHighlightOnValidCells(x, y, [0, -2], false);
-            }
-         }
+         setPawnPositionsByColor(x, y, color);
       } else if (color === "b") {
-         //prevent attack from the front
-         if (!cellIsTakenByOpponent(x, y + 1)) {
-            setHighlightOnValidCells(x, y, [0, 1], false);
-         }
-         //allow diagonal attacks
-         if (cellIsTakenByOpponent(x - 1, y + 1)) {
-            setHighlightOnValidCells(x, y, [-1, 1], false);
-         }
-         if (cellIsTakenByOpponent(x + 1, y + 1)) {
-            setHighlightOnValidCells(x, y, [1, 1], false);
-         }
-         //first move for pawns have option of moving two spaces ahead
-         if (y === 1) {
-            //prevent two tile jump when a piece is in front of it
-            if (cellIsEmpty(x, y + 1)) {
-               setHighlightOnValidCells(x, y, [0, 2], false);
-            }
-         }
+         setPawnPositionsByColor(x, y, color);
       }
    }
 
@@ -100,11 +108,11 @@ const Chess = (function() {
          y += shiftCoordinates[1];
          if (coordinatesInBounds(x, y) && !pieceBlocking) {
             if (cellIsEmpty(x, y)) {
-               setHighlight(y * 8 + x);
+               setHighlight(convertCoordinatesToIndex(x, y));
             } else {
                pieceBlocking = true;
                if (cellIsTakenByOpponent(x, y)) {
-                  setHighlight(y * 8 + x);
+                  setHighlight(convertCoordinatesToIndex(x, y));
                } else {
                   break;
                }
@@ -148,47 +156,65 @@ const Chess = (function() {
       });
    }
 
-   function setMovesForPieces(fullPiece, x, y, color) {
-      const piece = fullPiece.substring(1, fullPiece.length);
-
-      if (piece === "p") {
+   function setMovesForPieces(piece, x, y, color) {
+      if (isPawn(piece)) {
          setPawnPositions(x, y, color);
       }
 
-      if (piece === "kn") {
+      if (isKnight(piece)) {
          setKnightPositions(x, y);
       }
 
-      if (piece === "b") {
+      if (isBishop(piece)) {
          setDiagonalPositions(x, y);
       }
 
-      if (piece === "r") {
+      if (isRook(piece)) {
          setHorizontalVerticalPositions(x, y);
       }
 
-      if (piece === "q") {
+      if (isQueen(piece)) {
          setDiagonalPositions(x, y);
          setHorizontalVerticalPositions(x, y);
       }
 
-      if (piece === "kg") {
+      if (isKing(piece)) {
          setKingPositions(x, y);
       }
    }
 
-   function highlightPossibleMoves(currentPiece) {
-      //get the class of the current piece
-      const piece = currentPiece.classList[1];
-      //get index of the current piece selected
-      const pieceIndex = parseInt(
-         currentPiece.parentNode.getAttribute("index")
-      );
-      //convert the piece index into x and y coordinates
-      const x = pieceIndex % BOARD_SIZE;
-      const y = Math.floor(pieceIndex / BOARD_SIZE);
+   function isPawn(piece) {
+      return piece.classList[1].substring(1, piece.length) === "p";
+   }
 
-      const color = piece.substring(0, 1);
+   function isKnight(piece) {
+      return piece.classList[1].substring(1, piece.length) === "kn";
+   }
+
+   function isBishop(piece) {
+      return piece.classList[1].substring(1, piece.length) === "b";
+   }
+
+   function isRook(piece) {
+      return piece.classList[1].substring(1, piece.length) === "r";
+   }
+
+   function isQueen(piece) {
+      return piece.classList[1].substring(1, piece.length) === "q";
+   }
+
+   function isKing(piece) {
+      return piece.classList[1].substring(1, piece.length) === "kg";
+   }
+
+   function highlightPossibleMoves(piece) {
+      //get index of the current piece selected
+      const pieceIndex = parseInt(piece.parentNode.getAttribute("index"));
+      //convert the piece index into x and y coordinates
+      const x = convertIndexToCoordinates(pieceIndex).x;
+      const y = convertIndexToCoordinates(pieceIndex).y;
+
+      const color = piece.classList[1].substring(0, 1);
       if (color === "w") {
          setMovesForPieces(piece, x, y, color);
       } else if (color === "b") {
@@ -196,7 +222,100 @@ const Chess = (function() {
       }
    }
 
+   function kingIsInCheck(x, y, shiftCoordinates, spanBoard) {
+      let pieceBlocking = false;
+      while (true) {
+         x += shiftCoordinates[0];
+         y += shiftCoordinates[1];
+         if (coordinatesInBounds(x, y) && !pieceBlocking) {
+            if (!cellIsEmpty(x, y)) {
+               pieceBlocking = true;
+               if (cellIsTakenByOpponent(x, y) && isOpponentKing(x, y)) {
+                  displayMessage("Check");
+                  setHighlight(convertCoordinatesToIndex(x, y));
+               } else {
+                  break;
+               }
+            }
+            if (!spanBoard) {
+               break;
+            }
+         } else {
+            break;
+         }
+      }
+   }
+
+   //TODO: check for when king moves into a checked position
+   function check(piece) {
+      const index = piece.parentNode.getAttribute("index");
+      const x = convertIndexToCoordinates(index).x;
+      const y = convertIndexToCoordinates(index).y;
+
+      if (isPawn(piece)) {
+         if (whiteTurn) {
+            //check white pawn attack
+            kingIsInCheck(x, y, [-1, -1], false);
+            kingIsInCheck(x, y, [1, -1], false);
+         } else {
+            //check black pawn attack
+            kingIsInCheck(x, y, [-1, 1], false);
+            kingIsInCheck(x, y, [1, 1], false);
+         }
+      }
+
+      if (isRook(piece) || isQueen(piece)) {
+         kingIsInCheck(x, y, [0, -1], true);
+         kingIsInCheck(x, y, [0, 1], true);
+         kingIsInCheck(x, y, [-1, 0], true);
+         kingIsInCheck(x, y, [1, 0], true);
+      }
+
+      if (isBishop(piece) || isQueen(piece)) {
+         kingIsInCheck(x, y, [-1, -1], true);
+         kingIsInCheck(x, y, [1, -1], true);
+         kingIsInCheck(x, y, [1, 1], true);
+         kingIsInCheck(x, y, [-1, 1], true);
+      }
+
+      if (isKnight(piece)) {
+         kingIsInCheck(x, y, [1, -2], false);
+         kingIsInCheck(x, y, [-1, -2], false);
+         kingIsInCheck(x, y, [-2, 1], false);
+         kingIsInCheck(x, y, [-2, -1], false);
+         kingIsInCheck(x, y, [2, 1], false);
+         kingIsInCheck(x, y, [2, -1], false);
+         kingIsInCheck(x, y, [1, 2], false);
+         kingIsInCheck(x, y, [-1, 2], false);
+      }
+
+      if (isKing(piece)) {
+         kingIsInCheck(x, y, [-1, -1], false);
+         kingIsInCheck(x, y, [0, -1], false);
+         kingIsInCheck(x, y, [1, -1], false);
+         kingIsInCheck(x, y, [-1, 0], false);
+         kingIsInCheck(x, y, [1, 0], false);
+         kingIsInCheck(x, y, [-1, 1], false);
+         kingIsInCheck(x, y, [0, 1], false);
+         kingIsInCheck(x, y, [1, 1], false);
+      }
+   }
+
+   function checkMate() {}
+
+   function isOpponentKing(x, y) {
+      const kingClass = whiteTurn ? "bkg" : "wkg";
+      return board[
+         convertCoordinatesToIndex(x, y)
+      ].firstElementChild.classList.contains(kingClass);
+   }
+
+   function displayMessage(message) {
+      document.getElementById("message").innerText = message;
+   }
+
    function moveSelectedPiece(currentPiece) {
+      displayMessage("");
       const possibleMoves = document.querySelectorAll(".highlight");
 
       possibleMoves.forEach(function(target) {
@@ -214,14 +333,14 @@ const Chess = (function() {
                this.innerHTML = "";
                this.append(currentPiece);
                clearSelection();
+               check(currentPiece);
+               //game is won
+               //displayMessage(`${whiteTurn ? "White" : "Black"} wins!`);
+               //pieces.removeEventListener("click", this, true);
                whiteTurn = !whiteTurn;
             }
          });
       });
-   }
-
-   function gameIsWon() {
-      return document.querySelectorAll(".wkg, .bkg").length === 1;
    }
 
    function bindMovement() {
@@ -232,18 +351,14 @@ const Chess = (function() {
 
             if (playerTurn === currentPiece.classList[1].substring(0, 1)) {
                if (currentPiece.parentNode.classList.contains("selected")) {
+                  //this allows player to deselect a piece
                   clearSelection();
                } else {
+                  //this allows player to select a piece
                   clearSelection();
                   currentPiece.parentNode.classList.add("selected");
                   highlightPossibleMoves(currentPiece);
                   moveSelectedPiece(currentPiece);
-                  if (gameIsWon()) {
-                     document.getElementById("message").innerText = `${
-                        whiteTurn ? "White" : "Black"
-                     } wins!`;
-                     //pieces.removeEventListener("click", this, true);
-                  }
                }
             }
          });
